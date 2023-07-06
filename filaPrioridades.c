e543026eb32ff0f64a0c0a4e9bad49c92347fce8
almeidarayene <almeidarayene@gmail.com>
2023-07-05 21:46:46 -0300

    fila de prioridades

---
+++ filaPrioridades.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+typedef struct no{
+    int valor;
+    struct no *proximo;
+}No;
+
+void inserir_na_fila(No **fila, int num){
+    No *aux, *novo = malloc(sizeof(No));
+    if(novo){
+        novo->valor=num;
+        novo->proximo =NULL;
+        if(*fila ==NULL){
+            *fila = novo;
+        }
+        else{
+            aux = *fila;
+            while(aux->proximo)
+                aux= aux ->proximo;
+            aux-> proximo =novo;
+        }
+    }
+    else{ print("\nErro ao alocar memoria.\n");
+    }
+}
+    
+
+int main(){
+    No *fila = NULL;
+
+}
